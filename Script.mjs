3026348b1885d9c9c818c76555c831e574be1e3e
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-08 23:35:51 +0530

    altered the code

--- Script.mjs
+++ Script.mjs
@@ -1,6 +1,8 @@
 import fs from "fs";
 
+fs.mkdirSync("new_Folder");
+
 fs.writeFileSync(
-  `new_Folder/${new Date.toISOString()}`,
-  `Time_Stamp: ${Date.now()}`
+  `new_Folder/${new Date().toISOString()}.txt`,
+  `Time_Stamp: ${Date.now().toString()}`
 );
